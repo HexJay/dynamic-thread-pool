730ab8e28c345b59e71e3f0c82719b3bcc823c2d
Jay <HexJay1127@outlook.com>
2025-10-19 00:26:13 +0800

    init

---
+++ dynamic-thread-pool-spring-boot-starter/src/main/java/com/jovia/middleware/dynamic/thread/pool/sdk/config/DynamicThreadPoolAutoConfig.java
@@ -0,0 +1,9 @@
+package com.jovia.middleware.dynamic.thread.pool.sdk.config;
+
+/**
+ * @author Jay
+ * @date 2025/10/19 00:24
+ * @description 动态配置入口
+ */
+public class DynamicThreadPoolAutoConfig {
+}
---
+++ dynamic-thread-pool-spring-boot-starter/src/test/java/com/jovia/middleware/dynamic/thread/pool/ApiTest.java
@@ -0,0 +1,10 @@
+package com.jovia.middleware.dynamic.thread.pool;
+
+/**
+ * @author Jay
+ * @date 2025/10/19 00:25
+ * @description
+ */
+public class ApiTest {
+
+}
